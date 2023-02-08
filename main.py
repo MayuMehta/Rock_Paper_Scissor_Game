12720ad00f8d16dc6b6e371b2baf28ce8cbf0e73
MayuMehta <101469356+MayuMehta@users.noreply.github.com>
2023-02-08 21:29:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import random
-user_choice=input("Choose your weapon[Rock,Paper,Scissor]:")
+your_choice=input("Choose your weapon[Rock,Paper,Scissor]:")
 computer_choice= random.choice(['rock','paper','scissor'])
 
-user_choice=user_choice.lower()
+your_choice=your_choice.lower()
 
-print("Computer Choice:",computer_choice)
-print("User Choice:",user_choice)
+print("Computer Choice is : ",computer_choice)
+print("Your Choice is : ",your_choice)
 
-if(user_choice=="rock" and computer_choice=="rock"):
+if(your_choice=="rock" and computer_choice=="rock"):
   print("Draw")
-elif(user_choice=="rock" and computer_choice=="paper"):
+elif(your_choice=="rock" and computer_choice=="paper"):
   print("Computer won")
-elif(user_choice=="rock" and computer_choice=="scissor"):
+elif(your_choice=="rock" and computer_choice=="scissor"):
   print("User Won")
-elif(user_choice=="paper" and computer_choice=="rock"):
+elif(your_choice=="paper" and computer_choice=="rock"):
   print("User won")
-elif(user_choice=="paper" and computer_choice=="Paper"):
+elif(your_choice=="paper" and computer_choice=="Paper"):
   print("Draw")
-elif(user_choice=="paper" and computer_choice=="scissor"):
+elif(your_choice=="paper" and computer_choice=="scissor"):
   print("Computer won")
-elif(user_choice=="scissor" and computer_choice=="rock"):
+elif(your_choice=="scissor" and computer_choice=="rock"):
   print("Computer won")
-elif(user_choice=="scissor" and computer_choice=="paper"):
+elif(your_choice=="scissor" and computer_choice=="paper"):
   print("User won")
-elif(user_choice=="scissor" and computer_choice=="scissor"):
+elif(your_choice=="scissor" and computer_choice=="scissor"):
   print("Draw")
 else:
   print("Invalid input")
